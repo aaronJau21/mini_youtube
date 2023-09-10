09a410c6c92ad74d391f498837a30d8274d45109
aaronJau21 <aaronjau21@gmail.com>
2023-09-10 02:17:00 -0500

    created migrations and models

---
+++ backend/app/Models/User.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Model
+{
+    use HasFactory;
+    use HasApiTokens;
+
+    protected $fillable = [
+        'role',
+        'name',
+        'surname',
+        'email',
+        'password',
+        'image'
+    ];
+}
---
+++ backend/app/Models/Video.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Video extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'image',
+        'video_path'
+    ];
+
+    // One to Many
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    // One to One
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
